268b102f8bb30e9f08d9631e6fcffbc7ba2b5b3a
zack <1126146235@qq.com>
2025-10-16 15:25:19 +0800

    build(docs): 更新文档构建配置和静态资源
    - 修改 .gitignore 忽略

--- docs/.vitepress/realConfig/navbar.ts
+++ docs/.vitepress/realConfig/navbar.ts
@@ -1,39 +1,46 @@
-import { DefaultTheme } from 'vitepress';
+import { DefaultTheme } from "vitepress";
 
 export const nav: DefaultTheme.NavItem[] = [
   {
-    text: '首页',
-    link: '/' // 表示docs/index.md
+    text: "首页",
+    link: "/", // 表示docs/index.md
   },
   {
-    text: '我的博客搭建',
-    link: '/column/MyBlog/' 
+    text: "我的博客搭建",
+    link: "/column/MyBlog/",
   },
   {
-    text: '前端专栏',
+    text: "前端专栏",
     items: [
       {
-        text: 'HTML',
-        link: '/column/HTML/' // 表示docs/column/HTML/index.md
+        text: "HTML",
+        link: "/column/HTML/", // 表示docs/column/HTML/index.md
       },
       {
-        text: 'CSS',
-        link: '/column/HTML/' // 表示docs/column/CSS/index.md
+        text: "CSS",
+        link: "/column/HTML/", // 表示docs/column/CSS/index.md
       },
-       {
-        text: '数据结构与算法',
-        link: '/column/Algorithm/' // 对应docs/column/Algorithm下的index.md文件
-      }
-    ]
+      {
+        text: "数据结构与算法",
+        link: "/column/Algorithm/", // 对应docs/column/Algorithm下的index.md文件
+      },
+    ],
   },
   {
-    text: '关于我',
+    text: "关于我",
     items: [
-      { text: 'Github', link: 'https://github.com/RanGuMo' },
+      { text: "Github", link: "https://github.com/RanGuMo" },
       {
-        text: 'CSDN',
-        link: 'https://blog.csdn.net/qq_43322436?spm=1010.2135.3001.5343'
-      }
-    ]
-  }
+        text: "CSDN",
+        link: "https://blog.csdn.net/qq_43322436?spm=1010.2135.3001.5343",
+      },
+    ],
+  },
+  {
+    text: "其他站点",
+    items: [
+      { text: "VitePress文档搭建", link: "https://vitepress.yiov.top" },
+      { text: "VuePress文档搭建", link: "https://vuepress.yiov.top" },
+    ],
+  },
 ];
